0473aa194e93cc73baa4188ab3f000c00a70d8ad
Jill Oakes <jericho_rules@hotmail.com>
2022-04-19 23:06:57 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace OpenWeatherMapExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            Console.WriteLine("Welcome to the OpenWeatherMap weather center.");
+            Console.WriteLine();
+            
+            Console.WriteLine("To retrieve your current weather status, enter your API key: ");
+            var key = Console.ReadLine();
+            Console.WriteLine();
+
+            while (true)
+            {
+                Console.WriteLine("Enter the city: ");
+                var city = Console.ReadLine();
+                Console.WriteLine($"Here are the current conditions in {city}: ");
+
+                var wxURL = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}&units=imperial";
+
+                var response = client.GetStringAsync(wxURL).Result;
+                var formattedResponse = JObject.Parse(response).GetValue("main").ToString();
+                Console.WriteLine(formattedResponse);
+
+                Console.WriteLine();
+                Console.WriteLine("-------------------------");
+                Console.WriteLine();
+
+                Console.WriteLine("Choose another city?  yes/no");
+                var userInput = Console.ReadLine();
+                Console.WriteLine();
+
+
+                if (userInput.ToLower() == "no")
+                {
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
+            }         
+        }
+    }
+}
