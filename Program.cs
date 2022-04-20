ffc0b239ffdc57380adc3eb51ddab13596e3296f
Jill Oakes <jericho_rules@hotmail.com>
2022-04-20 12:10:36 -0400

    Internalize API key

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
+using System.IO;
 using System.Net.Http;
 
 namespace OpenWeatherMapExercise
@@ -11,10 +12,13 @@ namespace OpenWeatherMapExercise
             var client = new HttpClient();
             Console.WriteLine("Welcome to the OpenWeatherMap weather center.");
             Console.WriteLine();
-            
-            Console.WriteLine("To retrieve your current weather status, enter your API key: ");
-            var key = Console.ReadLine();
-            Console.WriteLine();
+
+            //Console.WriteLine("To retrieve your current weather status, enter your API key: ");
+            //var key = Console.ReadLine();
+            //Console.WriteLine();
+
+            var key = File.ReadAllText("appsettings.json");
+            var api = JObject.Parse(key).GetValue("DefaultKey").ToString();
 
             while (true)
             {
@@ -22,7 +26,7 @@ namespace OpenWeatherMapExercise
                 var city = Console.ReadLine();
                 Console.WriteLine($"Here are the current conditions in {city}: ");
 
-                var wxURL = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}&units=imperial";
+                var wxURL = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}&units=imperial";
 
                 var response = client.GetStringAsync(wxURL).Result;
                 var formattedResponse = JObject.Parse(response).GetValue("main").ToString();
